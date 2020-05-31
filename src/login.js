e7aca32e271924ebf6258706efc6b61fd6bafd62
shivansh98 <66006609+shivansh98@users.noreply.github.com>
2020-05-31 23:30:55 +0530

    Update login.js

--- src/login.js
+++ src/login.js
@@ -22,7 +22,7 @@ const Login = () => {
   const handleRequest = async () => {
     try {
       const response = await axios.post(
-        `${URL_}/signin`,
+        "http://backend-telemetry-dashboard-nodejs.apps.123.252.203.195.nip.io/signin",
         { email: user, password: pass },
         {
           headers: {
@@ -34,7 +34,7 @@ const Login = () => {
       console.log({ ...response.data });
 
       try {
-        const user = await axios.get(`${URL_}/`, {
+        const user = await axios.get("http://backend-telemetry-dashboard-nodejs.apps.123.252.203.195.nip.io/", {
           headers: {
             Authorization: `Bearer ${response.data.token}`,
           },
